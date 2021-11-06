ddcd8f7615c87a6d398582b2e1f11dfd179ce71a
Caio Ragazzi <ca.ragazzi@gmail.com>
2021-11-06 22:07:04 +0000

    first commit

---
+++ SimpleSignailR/Hubs/SignalRHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleSignailR.Hubs
+{
+    public class SignalRHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("--> User connected! " + Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Console.WriteLine("--> User disconnected! " + Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
